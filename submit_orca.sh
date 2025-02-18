1f6853d3643de764c9bb66e18cc9eaf7bdd34d08
antoine-frot <antoine.frot@orange.fr>
2025-02-18 14:34:25 +0100

    Remove the input if not stored

--- submit_orca.sh
+++ submit_orca.sh
@@ -208,6 +208,8 @@ if prompt_yes_no "Do you want to keep the input file?"; then
   mkdir -p "${root_dir}/$Input_directory"
   mv "${root_dir}/${input}" "${root_dir}/$Input_directory/${input}"
   echo "Input stored in $Input_directory."
+else
+  rm "${root_dir}/${input}"
 fi
 
 echo -e "${G}${#xyz_files[@]} submitted successfully.${NC}"
