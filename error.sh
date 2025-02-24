956a19fcba5d60bd06e1bf499db9ef87eebcbf06
Antoine Frot <antoine.frot@orange.fr>
2025-02-24 15:17:16 +0100

    can now use it from everywhere and remove error messages if file are in the working directory

--- error.sh
+++ error.sh
@@ -3,8 +3,15 @@
 Purpose: look if an orca job finished with an error
 '
 
+working_directory=$(pwd)
+cd "/home/afrot/Stage2025Tangui"
+
 for molecule in ${@:-Boranil*}; do
-  for source_path in "${molecule}/${molecule}-"*; do
-    grep -H "ORCA finished by error termination" "$source_path"/"${molecule}"*.out | awk -F"/" '{print $NF}'
-  done
+  if [ -d "$molecule" ]; then
+    for source_path in "${molecule}/${molecule}-"*; do
+      grep -H "ORCA finished by error termination" "$source_path"/"${molecule}"*.out | awk -F"/" '{print $NF}'
+    done
+  fi
 done
+
+cd $working_directory
