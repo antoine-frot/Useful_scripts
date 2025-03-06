57f8b226845a92e405ebcba9b4026a4351ad67e6
Antoine Frot <antoine.frot@orange.fr>
2025-03-06 22:21:22 +0100

    Default is now all molecules and not only the boranils

--- error.sh
+++ error.sh
@@ -6,7 +6,7 @@ Purpose: look if an orca job finished with an error
 working_directory=$(pwd)
 cd "/home/afrot/Stage2025Tangui"
 
-for molecule in ${@:-Boranil*}; do
+for molecule in ${@:-*}; do
   if [ -d "$molecule" ]; then
     for source_path in "${molecule}/${molecule}-"*; do
       grep -H "ORCA finished by error termination" "$source_path"/"${molecule}"*.out 2>/dev/null | awk -F"/" '{print $NF}'
