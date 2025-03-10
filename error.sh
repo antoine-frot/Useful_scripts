364aeca65d454e495142eca921ecad313c2a389d
Antoine Frot <antoine.frot@orange.fr>
2025-03-10 09:24:54 +0100

    add files from turbomole software

--- error.sh
+++ error.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : '
-Purpose: look if an orca job finished with an error
+Purpose: look if an ORCA or TURBOMOLE job finished with an error
 '
 
 working_directory=$(pwd)
@@ -10,6 +10,7 @@ for molecule in ${@:-*}; do
   if [ -d "$molecule" ]; then
     for source_path in "${molecule}/${molecule}-"*; do
       grep -H "ORCA finished by error termination" "$source_path"/"${molecule}"*.out 2>/dev/null | awk -F"/" '{print $NF}'
+      grep -H "ended abnormally" "$source_path"/*.out 2>/dev/null | awk -F"/" '{print $(NF-1), $NF}'
     done
   fi
 done
