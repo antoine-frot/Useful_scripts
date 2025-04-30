25113e39f8ca7ff78ca4048c4ddea645a34b0f7c
antoine-frot <antoine.frot@orange.fr>
2025-04-30 14:34:47 +0200

    Separate global aliases for different environment on bashrc for specific environment

--- bash_utility/bashrc_psmn.sh
+++ bash_utility/bashrc_psmn.sh
@@ -27,7 +27,7 @@ set -b
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
-
+#
 #############
 # Functions #
 #############
@@ -36,47 +36,11 @@ exp_molecules() {
     python3 -c "import sys; sys.path.append('/home/afrot/script/data_visualisation'); from experimental_data import MOLECULES_DATA; print(*[molecule['name'] for molecule in MOLECULES_DATA], sep='\n')"
 }
 
+
 #############
 # Variables #
 #############
 
-lake="${USER}@cl6226comp2.psmn.ens-lyon.fr"
-cascade="${USER}@s92node01.psmn.ens-lyon.fr"
-
-script="/home/afrot/script"
-slurm_utility="$script/slurm_utility"
-bash_utility="$script/bash_utility"
-workflow_tools="$script/workflow_tools"
-
 path_orca="/Xnfs/chimie/debian11/orca/orca_6_0_1"
 TURBODIR=/home/rrullan/TmoleX2024/TURBOMOLE
 source $TURBODIR/Config_turbo_env
-
-C0='CC3 CC5 CC7 CC9 CN4 CN6 CN8'
-C1='CN3 CN5 CN7 CN9'
-
-###########
-# Aliases #
-###########
-
-alias cputime="sreport -t Hour cluster AccountUtilizationByUser Start=2025-01-01 Users=$USER"
-alias sq='squeue -u $USER -o "%.8i %.61j %.6M"; echo "Number of jobs running: $(($(squeue --me --noheader | wc -l)))"'
-alias sqa='squeue -u $USER; echo "Number of jobs running: $(($(squeue --me --noheader | wc -l)))"'
-alias ls='ls --color'
-
-alias scancelall="$slurm_utility/scancelall.sh"
-alias scancelname="python3 $slurm_utility/scancelname.py"
-
-alias get_geom="$workflow_tools/get_geom.sh"
-alias get_transition="python3 $workflow_tools/get_transition.py"
-alias get_chiroptic="python3 $workflow_tools/get_chiroptic.py"
-
-alias rename="$bash_utility/rename.sh"
-alias rename_file="$bash_utility/rename_file.sh"
-alias remove_pattern="python3 $bash_utility/remove.py"
-
-alias submit_orca="$script/submit_orca.sh"
-alias submit_turbomole="$script/submit_turbomole.sh"
-
-
-alias molden="/home/ssteinma/bin/molden"
