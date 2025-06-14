91f9053796a67b7edf3a2d92b4a3f2c29cc3a33f
antoine-frot <antoine.frot@orange.fr>
2025-06-14 13:39:54 +0200

    Change goto to a bashrc function, because the function approach is cleanest since cd in functions naturally affects your current shell, and you avoid the complexity of sourcing vs executing entirely.

--- bash_utility/configuration_files/bash_aliases.sh
+++ bash_utility/configuration_files/bash_aliases.sh
@@ -26,6 +26,23 @@ denis_molecules() {
     python3 -c "import sys; sys.path.append('$path_to_git/data_visualisation'); from experimental_data import DENIS_MOLECULES; print(*[molecule for molecule in DENIS_MOLECULES], sep='\n')"
 }
 
+goto() {
+    if [[ $# -ne 1 || "$1" != *"-"* ]]; then
+        echo "Usage: goto molecule-method" >&2
+        return 1
+    fi
+    
+    local molecule="${1%%-*}"
+    local target_dir="$molecule/$1"
+    
+    if [[ -d "$target_dir" ]]; then
+        cd "$target_dir"
+    else
+        echo "Error: Directory '$target_dir' does not exist" >&2
+        return 1
+    fi
+}
+
 #############
 # Variables #
 #############
