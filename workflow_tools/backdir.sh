01b76870febe8046a2b8f85834689542c34103fa
Frot Antoine <afrot@taz.d5.icgm.fr>
2025-10-10 16:18:55 +0200

    Enhance backup file

--- workflow_tools/backdir.sh
+++
@@ -1,5 +0,0 @@
-# Got to the directory you want to copy and run this script
-dir=$(pwd)
-main_dir=$(dirname $dir)
-rsync -avz --rsync-path="mkdir -p $main_dir && rsync" $dir afrot@Coloquinte.d5.icgm.fr:$main_dir
-
---
+++ workflow_tools/backup_ICGM.sh
@@ -0,0 +1,21 @@
+# Go to the directory you want to copy on the other hosts and run this script
+dir=$(pwd)
+main_dir=$(dirname "$dir")
+hostnames=("Coloquinte.d5.icgm.fr" "taz")
+current_hostname=$(hostname)
+answer=true
+while $answer; do
+	read -n 1 -r -p "$(echo -e "Are you sure you want to backup $dir (y/n)?")" answer
+	echo
+	case "$answer" in
+	    [yY]) answer=false ;;
+	    [nN]) exit 0 ;;
+	    *) echo -e "Please answer y or n." ;;
+	esac
+done
+
+for hostname in "${hostnames[@]}"; do
+  if [ "$hostname" != "$current_hostname" ]; then
+    rsync -avz --exclude='WAVECAR' --rsync-path="mkdir -p $main_dir && rsync" "$dir" afrot@"$hostname":"$main_dir"
+  fi
+done
