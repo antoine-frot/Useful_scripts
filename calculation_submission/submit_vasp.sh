a088d8c6be47eae39e301891c6231850511a0d72
Frot Antoine <afrot@taz.d5.icgm.fr>
2025-10-16 14:13:40 +0200

    Add a file containing the vasp version to avoid choosing every time

--- calculation_submission/submit_vasp.sh
+++ calculation_submission/submit_vasp.sh
@@ -38,22 +38,30 @@ fi
 job_name=$(echo "$relative_path" | tr '/' '-') # Replace slashes with hyphens
 
 ### VASP_version
-echo "Which VASP version?"
-available_versions=("6.5.0-impi" "6.4.3-gf-impi" "6.4.1" "6.3.2" "6.1.1_patched" "5.4.4-opt2")
-select version in "${available_versions[@]}"; do
-    case $REPLY in
-        1|2|3|4|5)
-	    export vasp_version=Vasp6/vasp.$version
-            break
-            ;;
-        6)
-	    export vasp_version=Vasp5/vasp.$version
-            break
-            ;;
-        *)
-            echo "Invalid choice. Try again."
-            ;;
-    esac
-done
+VASP_version_file=VASP_version.txt
+if [ -f $VASP_version_file ]; then
+    vasp_version=$(cat $VASP_version_file)
+    echo "VASP version: $vasp_version"
+else
+    echo "Which VASP version?"
+    available_versions=("6.5.0-impi" "6.4.3-gf-impi" "6.4.1" "6.3.2" "6.1.1_patched" "5.4.4-opt2")
+    select version in "${available_versions[@]}"; do
+        case $REPLY in
+            1|2|3|4|5)
+            vasp_version=Vasp6/vasp.$version
+                break
+                ;;
+            6)
+            vasp_version=Vasp5/vasp.$version
+                break
+                ;;
+            *)
+                echo "Invalid choice. Try again."
+                ;;
+        esac
+    done
+    echo $vasp_version > $VASP_version_file
+fi
+export vasp_version
 
 sbatch --job-name=$job_name /home/afrot/Useful_scripts/calculation_submission/sbatch_files/vasp_slurm.sh
