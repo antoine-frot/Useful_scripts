fc44c17e5cdee38fd9e0f5a6dca165c0dc9e4faf
Frot Antoine <afrot@taz.d5.icgm.fr>
2025-10-28 15:09:20 +0100

    Restructured the input files with bash_profile and bash_logout

--- bash_utility/configuration_files/bash_variables.sh
+++ bash_utility/configuration_files/bash_variables.sh
@@ -2,6 +2,14 @@
 # Variables #
 #############
 
-export PYTHONPATH="$path_to_git:$PYTHONPATH"
-export EDITOR=vim # the default EDITOR is set to vim
 Submitted="$HOME/Submitted.txt"
+source ~/.git-prompt.sh
+python_env () {
+    if [ -n $VIRTUAL_ENV ]; then
+        echo "[${VIRTUAL_ENV##*/}]"
+    else
+        echo''
+    fi
+}
+PS1='$(python_env)$(__git_ps1 " (%s)") \h:\w\$ '
+# PS1='\u@\h:\w$(__git_ps1 " (%s)")$(python_env)$ '
