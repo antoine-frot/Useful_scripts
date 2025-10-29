789d59fdd58311b60c82043628a9943d9ee4cae2
Frot Antoine <afrot@taz.d5.icgm.fr>
2025-10-29 11:35:39 +0100

    make INPUTS a global variable for script vasp_clear

--- bash_utility/configuration_files/bashrc_taz.sh
+++ bash_utility/configuration_files/bashrc_taz.sh
@@ -3,7 +3,7 @@
 # export SYSTEMD_PAGER=
 
 #export INPUTS="INCAR POSCAR POTCAR KPOINTS VASP_version.txt"
-INPUTS="INCAR POSCAR POTCAR KPOINTS VASP_version.txt"
+export INPUTS="INCAR POSCAR POTCAR KPOINTS VASP_version.txt"
 local=afrot@coloquinte.d5.icgm.fr:/home/afrot
 alias activate="source $HOME/virtual_env_python/bin/activate"
 activate
