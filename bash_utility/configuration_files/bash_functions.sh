4401979caa8307bd68b122dcadc8dfdfd0837d58
Frot Antoine <afrot@taz.d5.icgm.fr>
2025-10-30 11:23:12 +0100

    Adapt goto for new architecture

--- bash_utility/configuration_files/bash_functions.sh
+++ bash_utility/configuration_files/bash_functions.sh
@@ -19,28 +19,11 @@ mcp()
     echo "${*:2}" | xargs -n 1 cp $1
 }
 
-goto() {
-    if [[ $# -ne 1 || "$1" != *"-"* ]]; then
-        echo "Usage: goto molecule-method" >&2
-        return 1
-    fi
-    
-    local molecule="${1%%-*}"
-    local target_dir="$molecule/$1"
-    
-    if [[ -d "$target_dir" ]]; then
-        cd "$target_dir"
-    else
-        echo "Error: Directory '$target_dir' does not exist" >&2
-        return 1
-    fi
-}
-
-back_to_main() {
-	# Change direcoty to the closest directory containing MAIN_DIR
-	current_dir=$(pwd)
-	main_dir=""
-	temp_dir="$current_dir"
+get_main_dir () {
+	# Get the closest directory containing MAIN_DIR
+	local current_dir=$(pwd)
+	local main_dir=""
+	local temp_dir="$current_dir"
 	while [ "$temp_dir" != "/" ]; do
 	    if [ -f "$temp_dir/MAIN_DIR" ]; then
 		main_dir="$temp_dir"
@@ -51,7 +34,32 @@ back_to_main() {
 
 	if [ -z "$main_dir" ]; then
 	    echo "Error: MAIN_DIR not found in any parent directory. Go not go back to previous main directory" >&2
+        return 1
 	else
-	    cd $main_dir
+	    echo $main_dir
+        return 0
 	fi
+}
+
+goto() {
+    if [[ $# -ne 1 || "$1" != *"-"* ]]; then
+        echo "Usage: goto molecule-method" >&2
+        return 1
+    fi
+    
+    local target_dir=$(echo "$1" | sed 's/-/\//g')
+    local main_dir=$(get_main_dir) || exit 1
+    local full_path="$main_dir/$target_dir"
+    
+    if [[ -d "$full_path" ]]; then
+        cd "$full_path"
+    else
+        echo "Error: Directory '$full_path' does not exist" >&2
+        return 1
+    fi
+}
+
+back_to_main() {
+    local main_dir=$(get_main_dir) || exit 1
+    cd $main_dir
 }
