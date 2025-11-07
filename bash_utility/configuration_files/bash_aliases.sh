54bcdebeb6572a4d3f64302130730add2ed56be8
Frot Antoine <afrot@taz.d5.icgm.fr>
2025-11-07 14:16:29 +0100

    Remove generated_aliases.sh and the lines to generate the aliases in bash_aliases as now I'm working with a bin

--- bash_utility/configuration_files/bash_aliases.sh
+++ bash_utility/configuration_files/bash_aliases.sh
@@ -9,18 +9,3 @@ alias terrorisme="sreport -t Hour cluster AccountUtilizationByUser Start=2025-01
 alias ls='ls --color'
 alias glog='git log --all --decorate --oneline --graph'
 alias Submitted='vim ~/Submitted.txt'
-
-# Aliases for scripts in the git repository
-alias get_chiroptic="python3 $path_to_git/data_visualisation/get_chiroptic.py"
-
-# Generate aliases for all usefull scripts in workflow
-#OUTPUT_GENERATE_ALIASE="$HOME/.generated_aliases.txt"
-#DIRS_WITH_SCRIPTS_FOR_WORKFLOW=("bash_utility" "calculation_submission" "get_properties/orca" "slurm_utility" "workflow_tools" "workflow_tools/orca" "workflow_tools/geometry_tools" "workflow_tools/fcclasses3" "workflow_tools/vasp")
-#if [ -f "$OUTPUT_GENERATE_ALIASE" ]; then
-#    rm "$OUTPUT_GENERATE_ALIASE"
-#fi
-#for dir in "${DIRS_WITH_SCRIPTS_FOR_WORKFLOW[@]}"; do
-#    echo "# $dir" >> "$OUTPUT_GENERATE_ALIASE"
-#    bash "$configuration_files/generate_aliases.sh" "$path_to_git/$dir" >> "$OUTPUT_GENERATE_ALIASE"
-#done
-#source "$OUTPUT_GENERATE_ALIASE"
