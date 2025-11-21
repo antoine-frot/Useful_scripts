e0a427f78b950e42955a9c80618d880b1ac30c09
Frot Antoine <afrot@taz.d5.icgm.fr>
2025-11-21 13:00:18 +0100

    Add the OUTPUTS variable for the sake of time

--- bash_utility/configuration_files/bashrc_taz.sh
+++ bash_utility/configuration_files/bashrc_taz.sh
@@ -2,8 +2,8 @@
 # Uncomment the following line if you don't like systemctl's auto-paging feature:
 # export SYSTEMD_PAGER=
 
-#export INPUTS="INCAR POSCAR POTCAR KPOINTS VASP_version.txt"
 export INPUTS="INCAR POSCAR POTCAR KPOINTS VASP_version.txt"
+OUTPUTS="INCAR POSCAR POTCAR KPOINTS VASP_version.txt WAVECAR CHGCAR"
 local=afrot@coloquinte.d5.icgm.fr:/home/afrot
 potentiels="/home/sol/Vasp/potentiels/potpaw_PBE.64"
 alias activate="source $HOME/virtual_env_python/bin/activate"
