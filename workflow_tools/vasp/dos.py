731bc8ec1adadde1d5c72a561e0581a86e26a054
Antoine Frot (ICGM) <antoine.frot@orange.fr>
2025-11-24 17:42:04 +0100

    Create a script to plot a dos with projection on each elements and color adjusted to an element. Warning: problem in the legend

---
+++ workflow_tools/vasp/dos.py
@@ -0,0 +1,190 @@
+#! /home/afrot/virtual_env_python/bin/python3
+from pymatgen.io.vasp.outputs import Vasprun
+from pymatgen.electronic_structure.plotter import DosPlotter
+import matplotlib.pyplot as plt
+
+vesta_colors = {
+    "H":   "#ffcccc",
+    "He":  "#fce8ce",
+    "Li":  "#86df73",
+    "Be":  "#5ed77b",
+    "B":   "#1fa20f",
+    "C":   "#4c4c4c",
+    "N":   "#b0b9e6",
+    "O":   "#fe0300",
+    "F":   "#b0b9e6",
+    "Ne":  "#fe37b5",
+    "Na":  "#f9dc3c",
+    "Mg":  "#fb7b15",
+    "Al":  "#81b2d6",
+    "Si":  "#1b3bfa",
+    "P":   "#c09cc2",
+    "S":   "#fffa00",
+    "Cl":  "#31fc02",
+    "Ar":  "#cffec4",
+    "K":   "#a121f6",
+    "Ca":  "#5a96bd",
+    "Sc":  "#b563ab",
+    "Ti":  "#78caff",
+    "V":   "#e51900",
+    "Cr":  "#00009e",
+    "Mn":  "#a7089d",
+    "Fe":  "#b57100",
+    "Co":  "#0000af",
+    "Ni":  "#b7bbbd",
+    "Cu":  "#2247dc",
+    "Zn":  "#8f8f81",
+    "Ga":  "#9ee373",
+    "Ge":  "#7e6ea6",
+    "As":  "#74d057",
+    "Se":  "#9aef0f",
+    "Br":  "#7e3102",
+    "Kr":  "#fac1f3",
+    "Rb":  "#702eb0",
+    "Sr":  "#00ff00",
+    "Y":   "#94ffff",
+    "Zr":  "#00ff00",
+    "Nb":  "#73c2c9",
+    "Mo":  "#54b5b5",
+    "Tc":  "#3b9e9e",
+    "Ru":  "#248f8f",
+    "Rh":  "#0a7d8c",
+    "Pd":  "#006985",
+    "Ag":  "#c0c0c0",
+    "Cd":  "#ffd98f",
+    "In":  "#a67573",
+    "Sn":  "#9a8eb9",
+    "Sb":  "#9e63b5",
+    "Te":  "#d47a00",
+    "I":   "#940094",
+    "Xe":  "#429eb0",
+    "Cs":  "#57178f",
+    "Ba":  "#00c900",
+    "La":  "#5ac449",
+    "Ce":  "#ffffc7",
+    "Pr":  "#d9ffc7",
+    "Nd":  "#c7ffc7",
+    "Pm":  "#a3ffc7",
+    "Sm":  "#8fffc7",
+    "Eu":  "#61ffc7",
+    "Gd":  "#45ffc7",
+    "Tb":  "#30ffc7",
+    "Dy":  "#1fffc7",
+    "Ho":  "#00ff9c",
+    "Er":  "#00e675",
+    "Tm":  "#00d452",
+    "Yb":  "#00bf38",
+    "Lu":  "#00ab24",
+    "Hf":  "#4dc2ff",
+    "Ta":  "#4da6ff",
+    "W":   "#2194d6",
+    "Re":  "#267dab",
+    "Os":  "#266696",
+    "Ir":  "#175487",
+    "Pt":  "#d0d0e0",
+    "Au":  "#ffd123",
+    "Hg":  "#b8b8d0",
+    "Tl":  "#a6544d",
+    "Pb":  "#575961",
+    "Bi":  "#9e4fb5",
+    "Po":  "#ab5c00",
+    "At":  "#754f45",
+    "Rn":  "#428296",
+    "Fr":  "#420066",
+    "Ra":  "#007d00",
+    "Ac":  "#70abfa",
+    "Th":  "#00baff",
+    "Pa":  "#00a1ff",
+    "U":   "#008fff",
+    "Np":  "#0080ff",
+    "Pu":  "#006bff",
+    "Am":  "#545cf2",
+    "Cm":  "#785ce3",
+    "Bk":  "#8a4fe3",
+    "Cf":  "#a136d4",
+    "Es":  "#b31fd4",
+    "Fm":  "#b31fba",
+    "Md":  "#b30da6",
+    "No":  "#bd0d87",
+    "Lr":  "#c70066",
+    "Rf":  "#cc0059",
+    "Db":  "#d1004f",
+    "Sg":  "#d90045",
+    "Bh":  "#e00038",
+    "Hs":  "#e6002e",
+    "Mt":  "#eb0026",
+    "Ds":  "#eb0026",   # (same as Mt in scheme)
+    "Rg":  "#eb0026",
+    "Cn":  "#eb0026",
+    "Nh":  "#eb0026",
+    "Fl":  "#eb0026",
+    "Mc":  "#eb0026",
+    "Lv":  "#eb0026",
+    "Ts":  "#eb0026",
+    "Og":  "#eb0026",
+}
+
+def enable_scroll_zoom(figure):
+    """
+    Adds scroll-to-zoom functionality to a matplotlib figure.
+    """
+    def zoom_fun(event):
+        base_scale = 1.1
+        # Get the current axis under the mouse
+        ax = event.inaxes
+        if ax is None:
+            return
+        
+        # Determine scale factor (Zoom In or Out)
+        if event.button == 'up':
+            scale_factor = 1 / base_scale
+        elif event.button == 'down':
+            scale_factor = base_scale
+        else:
+            scale_factor = 1
+
+        # Get current limits
+        cur_xlim = ax.get_xlim()
+        cur_ylim = ax.get_ylim()
+        
+        # Get cursor position (we want to zoom relative to cursor)
+        xdata = event.xdata
+        ydata = event.ydata
+
+        # Calculate new limits for X
+        new_width = (cur_xlim[1] - cur_xlim[0]) * scale_factor
+        relx = (cur_xlim[1] - xdata) / (cur_xlim[1] - cur_xlim[0])
+        ax.set_xlim([xdata - new_width * (1 - relx), xdata + new_width * relx])
+
+        # Calculate new limits for Y
+        new_height = (cur_ylim[1] - cur_ylim[0]) * scale_factor
+        rely = (cur_ylim[1] - ydata) / (cur_ylim[1] - cur_ylim[0])
+        ax.set_ylim([ydata - new_height * (1 - rely), ydata + new_height * rely])
+
+        # Redraw the figure
+        ax.figure.canvas.draw()
+
+    # Connect the function to the scroll event
+    figure.canvas.mpl_connect('scroll_event', zoom_fun)
+
+vasprun = Vasprun("vasprun.xml", parse_projected_eigen=True)
+dos = vasprun.complete_dos # Total DOS + projection on atoms and orbitals
+
+plotter = DosPlotter()
+plotter.add_dos('Total DOS', dos)
+plotter.add_dos_dict(dos.get_element_dos())
+ax = plotter.get_plot()  # Get Axis object
+lines = ax.get_lines()
+# Customize colors and line widths
+for line in lines:
+    label = line.get_label()
+    if label in vesta_colors:
+        line.set_color(vesta_colors[label])
+    elif label == 'Total DOS':
+        line.set_color('#000000')
+    line.set_linewidth(1)
+
+plt.savefig("dos.png")  # Save figure
+fig = plt.gcf()        # Get current figure
+enable_scroll_zoom(fig)
+plt.show()               # Display
