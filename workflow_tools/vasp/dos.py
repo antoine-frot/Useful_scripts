1a8a55576c7a140887851c9593f3dbb432692011
Antoine Frot (ICGM) <antoine.frot@orange.fr>
2025-11-25 09:15:03 +0100

    Create a python_utility folder to store the general python helper functions

--- workflow_tools/vasp/dos.py
+++ workflow_tools/vasp/dos.py
@@ -2,6 +2,7 @@
 from pymatgen.io.vasp.outputs import Vasprun
 from pymatgen.electronic_structure.plotter import DosPlotter
 import matplotlib.pyplot as plt
+from python_utility.figure_enable_scroll_zoom import enable_scroll_zoom
 
 vesta_colors = {
     "H":   "#ffcccc",
@@ -123,49 +124,6 @@ vesta_colors = {
     "Ts":  "#eb0026",
     "Og":  "#eb0026",
 }
-
-def enable_scroll_zoom(figure):
-    """
-    Adds scroll-to-zoom functionality to a matplotlib figure.
-    """
-    def zoom_fun(event):
-        base_scale = 1.1
-        # Get the current axis under the mouse
-        ax = event.inaxes
-        if ax is None:
-            return
-        
-        # Determine scale factor (Zoom In or Out)
-        if event.button == 'up':
-            scale_factor = 1 / base_scale
-        elif event.button == 'down':
-            scale_factor = base_scale
-        else:
-            scale_factor = 1
-
-        # Get current limits
-        cur_xlim = ax.get_xlim()
-        cur_ylim = ax.get_ylim()
-        
-        # Get cursor position (we want to zoom relative to cursor)
-        xdata = event.xdata
-        ydata = event.ydata
-
-        # Calculate new limits for X
-        new_width = (cur_xlim[1] - cur_xlim[0]) * scale_factor
-        relx = (cur_xlim[1] - xdata) / (cur_xlim[1] - cur_xlim[0])
-        ax.set_xlim([xdata - new_width * (1 - relx), xdata + new_width * relx])
-
-        # Calculate new limits for Y
-        new_height = (cur_ylim[1] - cur_ylim[0]) * scale_factor
-        rely = (cur_ylim[1] - ydata) / (cur_ylim[1] - cur_ylim[0])
-        ax.set_ylim([ydata - new_height * (1 - rely), ydata + new_height * rely])
-
-        # Redraw the figure
-        ax.figure.canvas.draw()
-
-    # Connect the function to the scroll event
-    figure.canvas.mpl_connect('scroll_event', zoom_fun)
 
 vasprun = Vasprun("vasprun.xml", parse_projected_eigen=True)
 dos = vasprun.complete_dos # Total DOS + projection on atoms and orbitals
