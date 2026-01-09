2ca5d08cff3277f2f91bb454492c2843517b1f86
Antoine Frot (ICGM) <antoine.frot@orange.fr>
2026-01-09 17:59:43 +0100

    Add backup functionality for vasp output files in vasp_restart.sh

--- calculation_submission/vasp_restart.sh
+++ calculation_submission/vasp_restart.sh
@@ -13,4 +13,18 @@ if [ -f CONTCAR ]; then
         exit 1
     fi
 fi
+files=("OSZICAR" "OUTCAR" "XDATCAR")
+# Saved each file with a the suffix corresponding to the slurm file (slurm-%j.out)
+latest_slurm_file=$(ls -t slurm-*.out | head -n 1)
+if [ -n "$latest_slurm_file" ]; then
+    slurm_suffix="${latest_slurm_file#slurm-}"
+    slurm_suffix="${slurm_suffix%.out}"
+    for file in "${files[@]}"; do
+        if [ -f "$file" ]; then
+            cp "$file" "${file}_${slurm_suffix}"
+        fi
+    done
+else
+    echo "No slurm output files found. Skipping backup of files: ${files[@]}."
+fi
 submit_vasp
