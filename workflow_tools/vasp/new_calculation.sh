ff9e2c6bf9b2920ed992dd55c7521e08ee521e35
Antoine Frot (ICGM) <antoine.frot@orange.fr>
2026-01-16 12:43:54 +0100

    Add new_calculation.sh script for creating new direcotry from an existing calculation

---
+++ workflow_tools/vasp/new_calculation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to create new VASP calculation directories from an existing calculation
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <calculation_directory_1> <calculation_directory_2> ..."
+    exit 1
+fi
+
+for calc_dir in "$@"; do
+    mkdir -p $calc_dir
+    cp $INPUT_FILES $calc_dir
+    cp CONTCAR $calc_dir/POSCAR
