0113e1cb12b59224f3ad401379e7fa7f851fa0dc
tornado <antoine.frot@orange.fr>
2026-02-11 10:41:17 +0100

    Updated the goto function

--- bash_utility/configuration_files/bash_functions.sh
+++ bash_utility/configuration_files/bash_functions.sh
@@ -42,7 +42,7 @@ get_main_dir () {
 }
 
 goto() {
-    if [[ $# -ne 1 || "$1" != *"-"* ]]; then
+    if [[ $# -ne 1 ]]; then
         echo "Usage: goto calculation-name" >&2
         return 1
     fi
